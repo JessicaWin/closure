e455bebf60fb745ea81e6e905462584203f3ba26
Jessica <baihechenlu@126.com>
2018-12-22 09:52:11 +0800

    closure init commit

---
+++ closure/src/main/java/goog/closure/App.java
@@ -0,0 +1,13 @@
+package goog.closure;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
